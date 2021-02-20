f60ee01755cd8fd8685c4a4a75bbd7e875b3ae62
Abdulov Azad <abdulov.azad@yandex.ru>
2021-02-20 18:04:57 +0300

    made mains throw

--- servlet/src/main/java/ServletApplication.java
+++ servlet/src/main/java/ServletApplication.java
@@ -1,34 +1,8 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class ServletApplication {
 
-    private static final Logger LOGGER;
-
-    static {
-        LOGGER = Logger.getLogger(ServletApplication.class.getName());
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CounterServer server = new CounterServer();
-        try {
-            server.start();
-            server.join();
-        }
-        catch (Exception e) {
-            LOGGER.log(Level.WARNING, "Server bootup failed", e);
-        }
-        finally {
-            stopServer(server);
-        }
-    }
-
-    private static void stopServer(CounterServer server) {
-        try {
-            server.stop();
-        }
-        catch (Exception e) {
-            LOGGER.log(Level.WARNING, "Server shutdown failed", e);
-        }
+        server.start();
+        server.join();
     }
 }
