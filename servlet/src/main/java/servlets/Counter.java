cff65d71495d71f83ed2b0e09f6b0bb27a57ffdd
Denis Nuriakhmetov <d.nuriakhmetov@awatera.com>
2021-03-14 22:56:49 +0300

    part servlet

---
+++ servlet/src/main/java/servlets/Counter.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter {
+  private static final Counter INSTANCE = new Counter();
+  private final AtomicInteger counter;
+
+  public Counter() {
+    counter = new AtomicInteger();
+  }
+
+  public static Counter getInstance() {
+    return INSTANCE;
+  }
+
+  public int getValue() {
+    return counter.get();
+  }
+
+  public int addValue(int value) {
+    return counter.addAndGet(value);
+  }
+
+  public void resetValue() {
+    counter.set(0);
+  }
+
+  public int increment() {
+    return counter.getAndIncrement();
+  }
+}
