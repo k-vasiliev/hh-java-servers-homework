cbf5b664e417edfec39d4271d4ac720be3fa4343
Ilya Safonov <29283825+Sparkymann@users.noreply.github.com>
2022-04-02 00:27:11 +0300

    Using annotations for params validation

--- jersey/src/main/java/resource/CounterResource.java
+++ jersey/src/main/java/resource/CounterResource.java
@@ -4,6 +4,10 @@ import dto.CounterDTO;
 import entity.Counter;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +17,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Path("/counter")
 public class CounterResource {
@@ -34,34 +37,30 @@ public class CounterResource {
 
     @POST
     @Path("/clear")
-    public Response clearCounter(@CookieParam(value = "hh-auth") String authParam) {
-        boolean isAuthorized = Optional.ofNullable(authParam)
-                .map(String::length)
-                .map(length -> length > 10)
-                .orElse(false);
-        if (isAuthorized) {
-            Counter.set(0);
-            return Response.ok().build();
-        } else {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
+    public Response clearCounter(
+            @CookieParam(value = "hh-auth")
+            @NotBlank
+            @Pattern(regexp = ".{11,}")
+            String authParam
+    ) {
+        Counter.set(0);
+        return Response.ok().build();
     }
 
+    //вроде в задании нет требований про валидацию, но, мне кажется, лучше это сделать
+    //и отрицательные значения отпинывать, если очень хочется увеличивать счетчик больше чем на 1, то лучше параметр в post-запрос добавить
     @DELETE
-    public Response decrementCounter(@HeaderParam(value = SUBTRACTION_VALUE_HEADER)Integer subtractionValue) {
-        if (subtractionValue == null) {
-            return Response.status(HttpServletResponse.SC_BAD_REQUEST, SUBTRACTION_VALUE_HEADER + " header is required").build();
+    public Response decrementCounter(
+            @HeaderParam(value = SUBTRACTION_VALUE_HEADER)
+            @NotNull(message = SUBTRACTION_VALUE_HEADER + " header is required")
+            @Min(value = 0, message = SUBTRACTION_VALUE_HEADER + " header must contain non-negative integer")
+            Integer subtractionValue
+    ) {
+        if (Counter.get() < subtractionValue) {
+            return Response.status(HttpServletResponse.SC_CONFLICT, SUBTRACTION_VALUE_HEADER + " header contains value greater then current counter value").build();
         } else {
-            //вроде в задании нет требований про валидацию, но, мне кажется, лучше это сделать
-            //и отрицательные значения отпинывать, если очень хочется увеличивать счетчик больше чем на 1, то лучше параметр в post-запрос добавить
-            if (subtractionValue < 0) {
-                return Response.status(HttpServletResponse.SC_BAD_REQUEST, SUBTRACTION_VALUE_HEADER + " header must contain positive integer").build();
-            } else if (Counter.get() < subtractionValue) {
-                return Response.status(HttpServletResponse.SC_CONFLICT, SUBTRACTION_VALUE_HEADER + " header contains value greater then current counter value").build();
-            } else {
-                Counter.addAndGet(-subtractionValue);
-                return Response.ok().build();
-            }
+            Counter.addAndGet(-subtractionValue);
+            return Response.ok().build();
         }
     }
 
