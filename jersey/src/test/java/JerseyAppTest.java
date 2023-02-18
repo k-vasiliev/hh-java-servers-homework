bf897eb14b0e4adac975a291caf459eb353499a0
bannovdaniil <baddan@mail.ru>
2023-02-19 01:22:27 +0300

    7. more time for test on gitHub

--- jersey/src/test/java/JerseyAppTest.java
+++ jersey/src/test/java/JerseyAppTest.java
@@ -214,7 +214,7 @@ public class JerseyAppTest {
   private static void waitUntilServerUp() {
     Awaitility.await()
         .atMost(10, TimeUnit.SECONDS)
-        .pollInterval(500, TimeUnit.MILLISECONDS)
+        .pollInterval(1000, TimeUnit.MILLISECONDS)
         .until(JerseyAppTest::isServerUp, IsEqual.equalTo(true));
   }
 
