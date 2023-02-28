5e400cc4709d1059619ca7fef7783f3e0a9090d3
Alexandr Toritsin <tori.bicks@gmail.com>
2023-03-01 01:25:52 +0300

    pass jetty tests

--- servlet/src/main/java/CounterServlet.java
+++ servlet/src/main/java/CounterServlet.java
@@ -1,4 +1,3 @@
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
@@ -23,17 +22,33 @@ public class CounterServlet extends HttpServlet {
       cookies[0].getValue().length() >= 10;
   }
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  public PrintWriter getPrintWriter(HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
-    PrintWriter writer = resp.getWriter();
+    return resp.getWriter();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter writer = getPrintWriter(resp);
 
     switch (req.getRequestURI()) {
       case "/status" -> writer.print(resp.getStatus());
-      case "/counter" -> {
-        System.out.println("Get request.");
-        System.out.println("Counter value: " + counter);
-        System.out.println("------------------");
+      case "/counter" -> writer.print(counter);
+    }
+    writer.flush();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter writer = getPrintWriter(resp);
+
+    switch (req.getRequestURI()) {
+      case "/counter" -> writer.print(counter.incrementAndGet());
+      case "/counter/clear" -> {
+        if (cookieIsValid(req.getCookies())) {
+          counter.getAndSet(0);
+        } else resp.setStatus(400);
+
         writer.print(counter);
       }
     }
@@ -41,49 +56,14 @@ public class CounterServlet extends HttpServlet {
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.setContentType("text/plain");
-    PrintWriter writer = resp.getWriter();
-
-    switch (req.getRequestURI()) {
-      case "/counter" -> {
-        System.out.println("Post request.");
-        System.out.println("Counter before increase: " + counter.get());
-        writer.print(counter.incrementAndGet());
-        System.out.println("Counter after increase: " + counter.get());
-        System.out.println("------------------");
-      }
-
-      case "/counter/clear" -> {
-        System.out.println("Clear request.");
-
-        if (cookieIsValid(req.getCookies())) {
-          System.out.println("Cookie name: " + req.getCookies()[0].getName());
-          System.out.println("Cookie value: " + req.getCookies()[0].getValue());
-          counter.getAndSet(0);
-          System.out.println("Counter decreased!");
-        } else {
-          resp.setStatus(400);
-          System.out.println("Counter did not decreased!");
-
-        }
-        writer.print(counter);
-        writer.flush();
-      }
-    }
-  }
-
-  @Override
-  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.setContentType("text/plain");
-    PrintWriter writer = resp.getWriter();
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter writer = getPrintWriter(resp);
 
     int headerValue = Integer.parseInt(req.getHeader("Subtraction-Value"));
-    System.out.println("Delete request.");
-    System.out.println("Counter before delete: " + counter);
-    counter.set(counter.get() - headerValue);
-    System.out.println("Counter after delete: " + counter);
-    System.out.println("--------------------");
+    for (int i = 0; i < headerValue; i++) {
+      counter.decrementAndGet();
+    }
     writer.print(counter);
+    writer.flush();
   }
 }
